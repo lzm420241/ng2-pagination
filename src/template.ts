29c44538c08124f0d9afd5d5246d4f239ba2f1d0
zhiming <lzm420241@gmail.com>
2016-09-26 13:58:00 +0800

    修改样式

--- src/template.ts
+++ src/template.ts
@@ -7,16 +7,16 @@ export const DEFAULT_TEMPLATE = `
     <div #template>
         <ng-content></ng-content>
     </div>
-    <ul class="ng2-pagination" 
-        role="navigation" 
-        aria-label="Pagination" 
+    <ul class="ng2-pagination"
+        role="navigation"
+        aria-label="Pagination"
         *ngIf="!hasTemplate && !(autoHide && pages.length <= 1)">
 
-        <li class="pagination-previous" [class.disabled]="isFirstPage()" *ngIf="directionLinks"> 
+        <li class="pagination-previous prev" [class.disabled]="isFirstPage()" *ngIf="directionLinks">
             <a *ngIf="1 < getCurrent()" (click)="previous()" aria-label="Next page">
-                Previous <span class="show-for-sr">page</span>
+                上一页 <span class="show-for-sr">page</span>
             </a>
-            <span *ngIf="isFirstPage()">Previous <span class="show-for-sr">page</span></span>
+            <span *ngIf="isFirstPage()">上一页 <span class="show-for-sr">page</span></span>
         </li>
 
         <li [class.current]="getCurrent() === page.value" *ngFor="let page of pages">
@@ -26,15 +26,15 @@ export const DEFAULT_TEMPLATE = `
             </a>
             <div *ngIf="getCurrent() === page.value">
                 <span class="show-for-sr">You're on page</span>
-                <span>{{ page.label }}</span> 
+                <span>{{ page.label }}</span>
             </div>
         </li>
 
-        <li class="pagination-next" [class.disabled]="isLastPage()" *ngIf="directionLinks">
+        <li class="pagination-next next" [class.disabled]="isLastPage()" *ngIf="directionLinks">
             <a *ngIf="!isLastPage()" (click)="next()" aria-label="Next page">
-                Next <span class="show-for-sr">page</span>
+                下一页 <span class="show-for-sr">page</span>
             </a>
-            <span *ngIf="isLastPage()">Next <span class="show-for-sr">page</span></span>
+            <span *ngIf="isLastPage()">下一页 <span class="show-for-sr">page</span></span>
         </li>
 
     </ul>
@@ -42,11 +42,9 @@ export const DEFAULT_TEMPLATE = `
 
 export const DEFAULT_STYLES = `
 .ng2-pagination {
-  margin-left: 0;
-  margin-bottom: 1rem; }
-  .ng2-pagination::before, .ng2-pagination::after {
-    content: ' ';
-    display: table; }
+  margin-top: 10px;
+  text-align: right;
+  margin-bottom: 70px;}
   .ng2-pagination::after {
     clear: both; }
   .ng2-pagination li {
@@ -55,45 +53,38 @@ export const DEFAULT_STYLES = `
     -ms-user-select: none;
     font-size: 0.875rem;
     margin-right: 0.0625rem;
-    border-radius: 0; }
+    border: 1px solid #333;
+    color: #333;
+    height: 30px;
+    border-radius: 5px;
+    line-height: 30px;
+    text-align: center;
+    margin: auto 3px;}
+    .prev, .next{
+      width: 60px;
+    }
   .ng2-pagination li {
     display: inline-block; }
   .ng2-pagination a,
   .ng2-pagination button {
-    color: #0a0a0a; 
+    color: #0a0a0a;
     display: block;
-    padding: 0.1875rem 0.625rem;
+    padding: 0 0.625rem;
     border-radius: 0; }
-    .ng2-pagination a:hover,
-    .ng2-pagination button:hover {
-      background: #e6e6e6; }
   .ng2-pagination .current {
-    padding: 0.1875rem 0.625rem;
-    background: #2199e8;
-    color: #fefefe;
+    padding: 0 0.625rem;
+    text-decoration: underline;
+    border: none;
     cursor: default; }
   .ng2-pagination .disabled {
-    padding: 0.1875rem 0.625rem;
-    color: #cacaca;
-    cursor: default; } 
-    .ng2-pagination .disabled:hover {
-      background: transparent; }
+    padding: 0 0.625rem;
+    border: 1px solid #bfbfbf;
+    color: #bfbfbf;
+    cursor: default; }
   .ng2-pagination .ellipsis::after {
     content: '…';
-    padding: 0.1875rem 0.625rem;
+    padding: 0 0.625rem;
     color: #0a0a0a; }
-
-.ng2-pagination .pagination-previous a::before,
-.ng2-pagination .pagination-previous.disabled::before { 
-  content: '«';
-  display: inline-block;
-  margin-right: 0.5rem; }
-
-.ng2-pagination .pagination-next a::after,
-.ng2-pagination .pagination-next.disabled::after {
-  content: '»';
-  display: inline-block;
-  margin-left: 0.5rem; }
 
 .ng2-pagination .show-for-sr {
   position: absolute !important;
@@ -101,4 +92,3 @@ export const DEFAULT_STYLES = `
   height: 1px;
   overflow: hidden;
   clip: rect(0, 0, 0, 0); }`;
- 
